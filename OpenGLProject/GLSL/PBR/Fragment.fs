6b431755e6e41d299c10600d0e9a32744c5d911b
Pham Duc <phamduc0701@gmail.com>
2019-06-19 22:24:33 +0700

    PBR Shading init

---
+++ OpenGLProject/GLSL/PBR/Fragment.fs
@@ -0,0 +1,110 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Light{
+	vec3 pos;
+	vec3 diffuse;
+};
+
+
+in vec2 TexCoords;
+in vec3 FragPos; 
+in vec3 Normal;
+
+uniform Light lights[4];
+uniform vec3 viewPos;
+uniform vec3 albedo;
+uniform float metallic;
+uniform float roughness;
+uniform float ao;
+
+const float PI = 3.14159265359;
+vec3 fresneslSchlick(float cosTheta,vec3 F0)
+{
+	return F0 + (1-F0) * pow(1.0f - cosTheta, 5.0f);
+}
+
+float DistributionGGX(vec3 N, vec3 H, float roughness) 
+{
+	float a = roughness * roughness;
+	float a2 = a*a;
+	float NdotH = max(dot(N,H),0.0f);
+	float NdotH2= NdotH*NdotH;
+
+	float num = a2;
+	float denom = (NdotH2*(a2-1.0f)+1.0f);
+	denom = PI * denom * denom;
+	
+	return num / denom; 
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness) 
+{
+	float r = roughness+1.0f;
+	float k = (r*r)/8.0f;
+
+	float num = NdotV;
+	float denom = NdotV*(1.0f-k) +k;
+	
+	return num / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+	float NdotV = max(dot(N,V),0.0f);
+	float NdotL = max(dot(N,L),0.0f);
+	float ggx1 = GeometrySchlickGGX(NdotL,roughness);
+	float ggx2 = GeometrySchlickGGX(NdotV,roughness);
+
+	return ggx1*ggx2;
+}
+
+void main()
+{
+	vec3 N = normalize(Normal);
+	vec3 V = normalize(viewPos - FragPos);
+
+
+	//Fresnel workflow
+	vec3 F0 = vec3(0.04f);
+	F0 = mix(F0, albedo, metallic);
+
+
+	vec3 Lo=vec3(0.0f);
+	for(int i = 0; i<4;i++)
+	{
+		vec3 L = normalize(lights[i].pos - FragPos);
+		vec3 H = normalize(V+L);
+
+		float distance = length(lights[i].pos-FragPos);
+		float attenuation = 1.0f / (distance*distance);
+		vec3 radiance = lights[i].diffuse * attenuation;
+
+		//Cook-Torrance BRDF
+		float D= DistributionGGX(N,H,roughness);
+		float G= GeometrySmith(N,V,L, roughness);
+		vec3 F = fresneslSchlick(max(dot(H,V),0.0f),F0);
+
+		vec3 kS=F;
+		vec3 kD=vec3(1.0f)-kS;
+		kD*=1.0f- metallic;
+
+		vec3 numerator = D*G*F;
+		float denominator = 4 * max(dot(N,V),0.0f)* max(dot(N, L), 0.0);
+		vec3 specular = numerator /max(denominator,0.001);
+
+		//Add to outgoing radiance Lo
+		//Add to outgoing radiance Lo
+        float NdotL = max(dot(N, L), 0.0);                
+        Lo += (kD * albedo / PI + specular) * radiance * NdotL; 
+	}
+
+	vec3 ambient = vec3(0.03) * albedo * ao;
+    vec3 color = ambient + Lo;
+	
+	//Color Correction
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0/2.2));  
+   
+    FragColor = vec4(color, 1.0);
+}
---
+++ OpenGLProject/Src/EntryPoint/PBRShading.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include "../../LoadingTexture.h"
+#include "../../Shader.h"
+#include "../../Camera.h"
+#include "../../Model.h"
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+void processInput(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+bool firstMouse = true;
+float lastX = 0.0f, lastY = 0.0f;
+
+float ratio = (float)4 / 3;
+Camera cam(glm::vec3(0.0f, 0.0f, 10.0f));
+
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMpAT, GL_TRUE);
+
+#endif // __APPLE__
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+
+	Shader PBRShader("GLSL/Vertex.vs","GLSL/PBR/Fragment.fs");
+	Shader light("GLSL/LightShade/Vertex.vs", "GLSL/LightShade/Fragment.fs");
+	Model sphere("Model/Sphere/LightSphere.obj");
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwPollEvents();
+		processInput(window);
+		glEnable(GL_DEPTH_TEST);
+
+		glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(35.0f), ratio, 0.1f, 1000.0f);
+
+		//Setting Lights 
+		//Setting Lights 
+		light.use();
+		light.setMat4fv("view", 1, GL_FALSE, cam.getView());
+		light.setMat4fv("projection", 1, GL_FALSE, projection);
+		glm::mat4 light_model(1.0f);
+		//T*R*S order
+		//T*R*S order
+		light_model = glm::translate(light_model, glm::vec3(0.5f,0.0f,4.0f));
+		light_model = glm::scale(light_model, glm::vec3(0.05f));
+		light.setMat4fv("model", 1, GL_FALSE, light_model);
+		light.setVec3("color", glm::vec3(1.0f));
+		sphere.Draw(light);
+
+
+		//Setting Models
+		//Setting Models
+		PBRShader.use();
+		PBRShader.setMat4fv("view", 1, GL_FALSE, cam.getView());
+		PBRShader.setMat4fv("projection", 1, GL_FALSE, projection);
+		PBRShader.setMat4fv("model", 1, GL_FALSE, glm::mat4(1.0f));
+		PBRShader.setVec3("viewPos", cam.getPos());
+		PBRShader.setVec3("albedo", glm::vec3(1.0f,0.0f,0.0f));
+		PBRShader.setFloat("roughness", 0.0f);
+		PBRShader.setFloat("metalness", 0.0f);
+		PBRShader.setVec3("lights[0].pos", glm::vec3(0.5f, 0.0f, 4.0f));
+		PBRShader.setVec3("lights[0].diffuse", glm::vec3(20.0f));
+		sphere.Draw(PBRShader);
+
+
+	
+
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	ratio = (float)width / height;
+	glViewport(0, 0, width, height);
+
+}
+
+void processInput(GLFWwindow *window) {
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cam.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cam.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cam.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cam.ProcessKeyboard(RIGHT, deltaTime);
+
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+	cam.ProcessMouseMovement(xoffset, yoffset, true, 0.1);
+}
